168891ce41d7b4be542cd55474dc34185939a31c
annyuan <annyuan@anns-mbp.media.mit.edu>
2016-10-24 10:08:51 -0400

    simplify names

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import { select, selectAll, event } from 'd3-selection'
 import { drag as d3drag } from 'd3-drag'
 
 const nodes = [
-  {"id": 1, "name": "server 1"},
-  {"id": 2, "name": "server 2"},
-  {"id": 3, "name": "server 3"},
-  {"id": 4, "name": "server 4"},
-  {"id": 5, "name": "server 5"},
-  {"id": 6, "name": "server 6"},
-  {"id": 7, "name": "server 7"},
-  {"id": 8, "name": "server 8"},
-  {"id": 9, "name": "server 9"}
+  {"id": 1, "name": "1"},
+  {"id": 2, "name": "2"},
+  {"id": 3, "name": "3"},
+  {"id": 4, "name": "4"},
+  {"id": 5, "name": "5"},
+  {"id": 6, "name": "6"},
+  {"id": 7, "name": "7"},
+  {"id": 8, "name": "8"},
+  {"id": 9, "name": "9"}
 ]
 
 const links = [
@@ -72,7 +72,7 @@ const dragEnded = d => {
 }
 
 const click = d => {
-  const target = { id: index, name: "server " + index }
+  const target = { id: index, name: index }
   nodes.push(target)
   links.push({ source: d, target })
   index++
