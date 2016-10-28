365ebf285b37285c491850c3f426d71b515a3ea2
annyuan <annyuan@dhcp-18-111-30-116.dyn.MIT.EDU>
2016-10-28 09:51:16 -0400

    no more click

--- src/index.js
+++ src/index.js
@@ -87,11 +87,6 @@ const dragEnded = d => {
   d.fy = undefined
 }
 
-const click = d => {
-  links.push({ source: d, target: nodes[10] })
-  update()
-}
-
 const drag = d3drag().on("start", dragStarted)
   .on("drag", dragged)
   .on("end", dragEnded)
@@ -111,7 +106,6 @@ const update = () => {
 
   const nodeEnter = node.enter().append("g")
     .attr("class", "node")
-    .on("click", click)
     .call(drag)
 
   nodeEnter.append("circle").attr("r", 2.5)
