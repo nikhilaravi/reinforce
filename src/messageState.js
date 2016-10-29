0794dff3411367459bc61f51662dd5e90beb88aa
annyuan <annyuan@Anns-MBP.home>
2016-10-29 06:35:19 -0400

    cleanup

--- src/messageState.js
+++ src/messageState.js
@@ -3,20 +3,7 @@ import nodes from './nodes'
 
 let current = []
 
-// messages each have an id, an orientation, and a user id associated, and a retweet id (if it is a retweet)
-
-// maybe the way it should work is, this component could reach out and collect messages from others. also collect their ids
-
-// then it sends these messages out 
-
-// each node subscribes to the message state
-
-// when a node hears a message from a user it's following, it records it (nodes record 3 cycles deep)
-
-// on the next cycle, nodes can retweet out something they agree with from the previous cycle
-// unless they were the originator
-
-// what goes into state?
+// what goes into state? on what basis should we have the agent learn?
 // how well their messages performed. 
 // what everyone else has been saying (up to 3 cycles deep)
 // maybe for now return random numbers for state
