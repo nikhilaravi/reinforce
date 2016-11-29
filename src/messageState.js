645e69a328cdeed4527ed3638de2a997cca2dd31
annyuan <annyuan@Anns-MBP.home>
2016-11-28 21:03:37 -0500

    saving memory

--- src/messageState.js
+++ src/messageState.js
@@ -4,6 +4,24 @@ import { Nodes } from './nodes'
 const maxCyclesInMemory = 5
 let memory = {}, current = []
 
+const updateMessageReach = (id, followersCount, retweet) => {
+	if(typeof memory[id] === 'undefined' && !retweet) {
+		memory[id] = {
+			reach: 0,
+			cycles: 0
+		}
+	}
+
+	if(typeof memory[id] !== 'undefined') {
+		if(memory[id].cycles > maxCyclesInMemory) {
+			delete memory[id]
+		} else {
+			memory[id].reach += followersCount
+			memory[id].cycles++			
+		}			
+	}
+}
+
 export default {
 	init() {
 		helpers.bindAll(this, [ "collectMessages", "emitMessages", "cycle" ])
@@ -18,34 +36,20 @@ export default {
 
 	collectMessages() {
 		for(let i=0; i<Nodes.length; i++) {
-			current.push(Object.assign(
+			let message = Object.assign(
 				Nodes[i].getMessage(), {
 					id: uuid.v4()
-				}))
+				})
+
+			current.push(message)
+
+			updateMessageReach(message.id, Nodes[i].followedBy.length, !!message.retweetID)
 		}
 	},
 
 	emitMessages() {
 		for(let i=0; i<Nodes.length; i++) {
 			Nodes[i].sendMessages(current)
-		}
-	},
-
-	updateMessageReach(id, followersCount, retweet) {
-		if(typeof memory[id] === 'undefined' && !retweet) {
-			memory[id] = {
-				reach: 0,
-				cycles: 0
-			}
-		}
-
-		if(typeof memory[id] !== 'undefined') {
-			if(memory[id].cycles > maxCyclesInMemory) {
-				delete memory[id]
-			} else {
-				memory[id].reach += followersCount
-				memory[id].cycles++			
-			}			
 		}
 	},
 
