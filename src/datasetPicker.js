84f36527599119dd4835a04b96eb4763c688e482
annyuan <annyuan@Anns-MBP.home>
2016-12-09 08:42:50 -0500

    toggle active state

--- src/datasetPicker.js
+++ src/datasetPicker.js
@@ -2,6 +2,17 @@ import mediator from './mediator'
 
 let pickerOpen = false
 
+const datasets = {
+	downTerrorism: {
+		nodes: 'downsampled_terrorism_nodes',
+		edges: 'downsampled_terrorism_edges'
+	},
+	toy: {
+		nodes: 'nodes_toy',
+		edges: 'edges_toy'
+	}
+}
+
 const closePicker = () => {
 	pickerOpen = false
 	updatePickerVisibility()
@@ -30,6 +41,14 @@ document.addEventListener("click", e => {
 		e.stopPropagation()
 		e.preventDefault()
 		togglePicker()
+
+		if(e.target.closest(".dropdown")) {
+			Array.prototype.forEach.call(e.target.closest(".dropdown").querySelectorAll(".option"), node => {
+				node.classList.remove("active")
+			})
+
+			e.target.classList.add("active")
+		}
 	} else {
 		closePicker()
 	}
