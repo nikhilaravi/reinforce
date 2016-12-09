37e5dc6e1600dacb14c34987acf785ba0b8e51fa
annyuan <annyuan@dhcp-18-111-27-212.dyn.mit.edu>
2016-12-09 10:47:10 -0500

    clean unused code

--- src/node.js
+++ src/node.js
@@ -27,22 +27,7 @@ export default class Node {
 		this.nextAction = null
 		this.retweeted = []
 
-		// this.maxMSE = beliefs.reduce((acc, curr) =>
-		// 	acc + Math.pow(-(1 / beliefs.length), 2), 0) / beliefs.length
-
 		this.cycleInterval = Math.round(Math.random() * maxCyclesInMemory)
-
-		// this.agent = new RL.DQNAgent(this, {
-	 //    update: 'qlearn',
-	 //    gamma: 0.9, // discount factor, [0, 1)
-	 //    epsilon: 0.2, // initial epsilon for epsilon-greedy policy, [0, 1)
-	 //    alpha: 0.01, // value function learning rate
-	 //    experience_add_every: 10, // number of time steps before we add another experience to replay memory
-	 //    experience_size: 5000, // size of experience replay memory
-	 //    learning_steps_per_iteration: 20,
-	 //    tderror_clamp: 1.0, // for robustness
-	 //    num_hidden_units: 100 // number of neurons in hidden layer
-	 //  })
 	}
 
 	set followedBy(newFollowedBy) { this._followedBy = newFollowedBy }
@@ -56,10 +41,6 @@ export default class Node {
 	set lastFollowing(newLastFollowing) { this._lastFollowing = newLastFollowing }
 
 	get lastFollowing() { return this._lastFollowing }
-
-	getNumStates() { return this.beliefs.length }
-
-	getMaxNumActions() { return this.beliefs.length }
 
 	getState() {
 		let counts = this.beliefs.reduce((acc, curr) => {
