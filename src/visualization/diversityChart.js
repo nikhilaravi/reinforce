a6682e79b2a50756b0e27bdbc6e94601769dfa6c
annyuan <annyuan@anns-mbp.media.mit.edu>
2016-12-13 12:09:24 -0500

    setup

--- src/visualization/diversityChart.js
+++ src/visualization/diversityChart.js
@@ -4,13 +4,11 @@ import { select } from 'd3-selection'
 import VisualizationBase from './visualizationBase'
 import helpers from '../helpers/helpers'
 const { dictToArray } = helpers
+import { initialDiversity, currentDiversity } from '../nodes'
 
 export class DiversityHistogram extends VisualizationBase {
   constructor(svg, width, height, testName) {
 		super(svg, width, height, testName)
-    
-    this.start = []
-    this.current = []
   }
 
   setup() {
@@ -18,20 +16,14 @@ export class DiversityHistogram extends VisualizationBase {
   }
 
   update(Nodes) {
-    var diversity_distribution = dictToArray(calculateDistribution(Nodes, 'diversity'), true)
     // diversity_distribution is an array of arrays of the form: [[diversity_score, num_nodes], [diversity_score, num_nodes], [diversity_score, num_nodes]]
     // the scores are in random order i.e. not in order of increasing/decreasing scores
 
-    if(!this.start.length) {
-      this.start = diversity_distribution
-    }
-
-    this.current = diversity_distribution
+    console.log(initialDiversity, currentDiversity)
   }
 
   clear() {
-    this.start = []
-    this.current = []
+
   }
 }
 
@@ -50,15 +42,4 @@ export class BeliefBarChart extends VisualizationBase {
   }
 }
 
-// create dict based on a property of the node
-function calculateDistribution(Nodes, prop) {
-  return Nodes.reduce((dict, node) => {
-    var belief = node[prop];
-    if (dict.hasOwnProperty(belief)) {
-      dict[belief] += 1
-    } else {
-      dict[belief] = 1
-    }
-    return dict
-  }, {})
-}
+
